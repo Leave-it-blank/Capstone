591b16f18454d35016c5a22a2859f79a531ceb09
blank <leaveitblank32@gmail.com>
2022-10-13 05:10:02 +0530

    watson api implemtation

---
+++ machinetranslation/tests/test_translator.py
@@ -0,0 +1,21 @@
+import unittest
+from translator import en_to_fr, fr_to_en
+"""
+Translation functions testing that uses watson api to translate
+"""
+class TestEnToFr(unittest.TestCase):
+    """
+    Translation functions test EN to FR
+    """
+    def test(self): 
+        self.assertEqual(en_to_fr("Hello"), "Bonjour") # test when 2 is given as input the output is 4.
+
+
+class TestFrToEn(unittest.TestCase):
+    """
+    Translation functions test FR to EN
+    """
+    def test(self): 
+        self.assertEqual(fr_to_en("Bonjour"), "Hello") # test when 2 is given as input the output is 4.
+unittest.main()
+
